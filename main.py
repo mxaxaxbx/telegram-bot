845beda1fdd0dda6d0c28dcce8deb342f233b839
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2023-04-12 11:59:59 -0500

    New response

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, Response
  
 app = Flask(__name__)
- 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>Welcome!</h1>"
+    if request.method == 'POST':
+        msg = request.get_json()
+        print(msg)
+       
+        return Response('ok', status=200)
+    else:
+        return "<h1>Welcome!</h1>"
  
 if __name__ == '__main__':
    app.run(debug=False, host='0.0.0.0')
