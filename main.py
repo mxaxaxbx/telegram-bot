c911153d22ea6ee3f4b78cb8624eef02e4f3fc8f
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2023-04-12 12:05:32 -0500

    prints added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print('hola')
+    print(request.method)
     if request.method == 'POST':
         msg = request.get_json()
         print(msg)
