f35793eeeac5f981b91de7368e9a40d718bed9a2
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2023-04-12 21:47:22 -0500

    Bisection added

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import NamedTuple, Text, List
 import os
 
 API_BASE = os.getenv("API_BASE", "https://framex-dev.wadrid.net/api/")
@@ -13,3 +13,57 @@ class Size(NamedTuple):
 
     width: int
     height: int
+
+class Color(NamedTuple):
+    """
+    8-bit components of a color
+    """
+
+    r: int
+    g: int
+    b: int
+
+class Video(NamedTuple):
+    """
+    That's a video from the API
+    """
+
+    name: Text
+    width: int
+    height: int
+    frames: int
+    frame_rate: List[int]
+    url: Text
+    first_frame: Text
+    last_frame: Text
+
+DISPLAY_SIZE = Size(int(480 * 1.5), int(270 * 1.5))
+BLACK = Color(0, 0, 0)
+
+def bisect(n, mapper, tester):
+    """
+    Runs a bisection.
+
+    - `n` is the number of elements to be bisected
+    - `mapper` is a callable that will transform an integer from "0" to "n"
+      into a value that can be tested
+    - `tester` returns true if the value is within the "right" range
+    """
+
+    if n < 1:
+        raise ValueError("Cannot bissect an empty array")
+
+    left = 0
+    right = n - 1
+
+    while left + 1 < right:
+        mid = int((left + right) / 2)
+
+        val = mapper(mid)
+
+        if tester(val):
+            right = mid
+        else:
+            left = mid
+
+    return mapper(right)
