1870ef094454df7bb5204c0541a7a314e27a9c71
Miguel A Arenas <miguel.arenas@sitidoctor.com>
2023-04-14 13:03:15 -0500

    testing project

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 
 from game import FrameXBisector, bisect
 
+import random
 import os
  
 app = Flask(__name__)
@@ -11,7 +12,8 @@ TOKEN = "6149126155:AAF6FUeYlPkZo9Q6dOhRmErkN0xBQtD8UpA"
 VIDEO_NAME = os.getenv(
     "VIDEO_NAME", "Falcon Heavy Test Flight (Hosted Webcast)-wbSwFU6tY1c"
 )
-
+global n
+n = 20
 
 def parse_message(message):
     print("message-->",message)
@@ -29,83 +31,70 @@ def confirm(title, chat_id, text):
     payload = {
         'chat_id': chat_id,
         'photo': text,
-        'caption': title
+        'caption': ''
     }
+    r = requests.post(url,json=payload)
+
+    # url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     # payload = {
-    #             'chat_id': chat_id,
-    #             'text': f"{title} - did the rocket launch yet?"
-    #             }
-   
+    #     'chat_id': chat_id,
+    #     'text': "¿se ha lanzado el cohete?"
+    # }
+    # r = requests.post(url,json=payload)
+
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': "¿se ha lanzado el cohete?",
+                'reply_markup': {'keyboard': [[{'text': 'si'}, {'text': 'no'}]]}
+    }
     r = requests.post(url,json=payload)
     print(r.json())
 
     return  f"{title} - did the rocket launch yet?"
+
+def confirm2(title, chat_id, text):
+
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    r = requests.post(url,json=payload)
+
+    return  f"{title} - did the rocket launch yet?"
  
+
 def tel_send_message(chat_id, text):
-    print('acaaa')
     bisector = FrameXBisector(VIDEO_NAME)
-    def mapper(n):
-        """
-        In that case there is no need to map (or rather, the mapping
-        is done visually by the user)
-        """
-
-        return n
-
-    def tester(n):
-        """
-        Displays the current candidate to the user and asks them to
-        check if they see wildfire damages.
-        """
-
-        bisector.index = n
-        # disp.fill(BLACK)
-        bisector.blit()
-        # pygame.display.update()
-
-        # return confirm(bisector.index, chat_id, text)
-    
     # culprit = bisect(bisector.count, mapper, tester)
-    culprit = 20
+    n = os.getenv('N', '20')
+    culprit = int(n)
     bisector.index = culprit
     img_base64 = bisector.blit()
-    confirm(title='Image', chat_id=chat_id, text=img_base64)
-    print('culprit', culprit)
-    # url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    # payload = {
-    #             'chat_id': chat_id,
-    #             'text': text
-    #             }
-   
-    # r = requests.post(url,json=payload)
+    print(img_base64)
+    if text != 'si':
+        confirm(title='Image', chat_id=chat_id, text=img_base64)
+        n = int(n) + random.randint(1000, 10000)
+        if n > 61695:
+            confirm2(title='', chat_id=chat_id, text='No se ha encontrado el lanzamiento del cohete')
+            n = 20
+        os.environ['N'] = str(n)    
+    else:
+        n = os.getenv('N')
+        confirm2(title='muajja', chat_id=chat_id, text='Se ha lanzadao el cohete en el frame ' + str(n))
+        n = 20
+        os.environ['N'] = str(n)
+
     return 'img_base64'
 
-def tel_send_image(chat_id):
-    bisector = FrameXBisector(VIDEO_NAME)
-    print(bisector)
-    # url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    # payload = {
-    #     'chat_id': chat_id,
-    #     'photo': "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-NkWPQU3-6dvelLDoZZ47CR_0iOnZvmI_WrCH1FrkBw&s",
-    #     'caption': "This is a sample image"
-    # }
- 
-    # r = requests.post(url, json=payload)
-    return 'r'
- 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         msg = request.get_json()
        
         chat_id,txt = parse_message(msg)
-        res = 'ok'
-        if txt == "hi":
-            res = tel_send_message(chat_id,"Hello!!")
-        elif txt == "image":
-            tel_send_image(chat_id)
-        else:
-            tel_send_message(chat_id,'from webhook')
+        res = tel_send_message(chat_id, txt)
        
         return Response(res, status=200)
     else:
